1ae8097233e676ee28321e5727ba71c0b96b6dbe
czogran <pawel.rawicki@gmail.com>
2022-12-11 09:46:17 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MenuModule} from "primeng/menu";
+import {ButtonModule} from "primeng/button";
+import {BreadcrumbModule} from "primeng/breadcrumb";
+import {MegaMenuModule} from "primeng/megamenu";
+import { PaperComponent } from './paper/paper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenuModule,
+    ButtonModule,
+    BreadcrumbModule,
+    MegaMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
